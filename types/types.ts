57a7d351479cf52f995e58c5b39236d2a3ddf45e
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-24 14:53:04 +0300

    add types folder

---
+++ types/types.ts
@@ -0,0 +1,37 @@
+//api'dan aldığım json dosyasını quicktype ile typlarını belirledim.
+export interface Characters {
+  id: number;
+  name: string;
+  status: Status;
+  species: Species;
+  type: string;
+  gender: Gender;
+  origin: Location;
+  location: Location;
+  image: string;
+  episode: string[];
+  url: string;
+  created: Date;
+}
+
+export enum Gender {
+  Female = "Female",
+  Male = "Male",
+  Unknown = "unknown",
+}
+
+export interface Location {
+  name: string;
+  url: string;
+}
+
+export enum Species {
+  Alien = "Alien",
+  Human = "Human",
+}
+
+export enum Status {
+  Alive = "Alive",
+  Dead = "Dead",
+  Unknown = "unknown",
+}

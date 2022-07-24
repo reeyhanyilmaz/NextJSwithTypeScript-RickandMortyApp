95f8e5e9f4d59e904ba0d4cabd4a67f077ed4b74
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-24 16:25:32 +0300

    add image optimization

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  //images error verdiği için yazdık, tekrar npm run dev dediğimizde ekrana basacak resimleri.
+  images: {
+    domains: ["rickandmortyapi.com"],
+
+    //image optimization için
+    loader: "custom",
+    path: "/"
+  }
 }
 
 module.exports = nextConfig

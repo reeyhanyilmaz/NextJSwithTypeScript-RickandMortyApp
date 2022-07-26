738d621989b3cec69b1d1d9f8bd12ea875002d4f
Reyhan Yılmaz <reyhanylmz1993@gmail.com>
2022-07-26 20:03:43 +0300

    netlify quotes

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,13 +35,13 @@ function Home() {
             Pick your favorite character and find out more about them. <br />
             <br />
             Rick and Morty is an American adult animated science fiction sitcom
-            created by Justin Roiland and Dan Harmon for Cartoon Network's
+            created by Justin Roiland and Dan Harmon for Cartoon Networks
             nighttime programming block Adult Swim. The series follows the
             misadventures of cynical mad scientist Rick Sanchez and his
             good-hearted but fretful grandson Morty Smith, who split their time
             between domestic life and interdimensional adventures. Roiland
             voices the eponymous characters, with Chris Parnell, Spencer Grammer
-            and Sarah Chalke voicing the rest of Rick and Morty's family. The
+            and Sarah Chalke voicing the rest of Rick and Mortys family. The
             series originated from an animated short parody film of Back to the
             Future, created by Roiland for Channel 101, a short-film festival
             co-founded by Harmon. Since its debut, the series has received
